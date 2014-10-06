f43b3d007c46bba75cc6533c9411a5b86b834294
tiduse22 <tiduse22@hotmail.com>
2014-10-06 12:45:26 +0200

    CharacterMovement

---
+++ New Unity Project 1/Assets/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(0, speed * Time.deltaTime, 0);
+            this.renderer.material.color = Color.red;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(0, -(speed * Time.deltaTime), 0);
+            this.renderer.material.color = Color.green;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-(speed * Time.deltaTime), 0, 0);
+            this.renderer.material.color = Color.yellow;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(speed * Time.deltaTime, 0, 0);
+            this.renderer.material.color = Color.black;
+        }
+        
+	}
+}
